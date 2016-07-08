d45e14dda38f03c6365740bb5315990dafcdd013
Tim Tregubov <tim@zingweb.com>
2016-07-08 18:59:22 -0400

    package updates and autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   stats: { colors: true },
@@ -17,16 +18,13 @@ module.exports = {
       loader: 'babel',
     },
     {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
-    },
-    {
       test: /\.scss/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader!postcss-loader'),
     },
       // You could also use other loaders the same way. I. e. the autoprefixer-loader
     ],
   },
+  postcss: [autoprefixer({ browsers: ['last 2 versions'] })],
   plugins: [
     new ExtractTextPlugin('bundle.css'),
   ],
