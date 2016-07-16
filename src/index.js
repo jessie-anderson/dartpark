538cbdfb220b606c7d980c69e474a46bbe217be8
Tim Tregubov <tim@zingweb.com>
2016-07-16 15:51:03 -0400

    style import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+import './style.scss';
+
 // entry point that just renders app
 // could be used for routing at some point
 ReactDOM.render(<App />, document.getElementById('main'));
