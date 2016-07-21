bcff07e04ffa726a3b7e6fbda48d445289cf0def
Jessie Anderson <Jessie@jessies-mbp-2.wireless.dartmouth.edu>
2016-07-21 17:08:47 -0400

    done with basic routing starter

--- src/components/app.js
+++ src/components/app.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Welcome />
+        {this.props.children}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 import App from './components/app';
 
 import './style.scss';
 
 // entry point that just renders app
 // could be used for routing at some point
-ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />
+  , document.getElementById('main'));
