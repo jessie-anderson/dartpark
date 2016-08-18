b06698a4b6c18d77776c6b3b537af86400c589a8
Benjamin Cooper <benjaminkagancooper@gmail.com>
2016-08-18 15:23:26 -0400

    update

--- src/components/renter/buy-spot.js
+++ src/components/renter/buy-spot.js
@@ -1,21 +1,22 @@
 import ResultItem from './spot-list-item';
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 import CardItemRender from './payment-render';
 
 
 // corresponds to FindSpot-Select on mockups
 
-const BuyItem = (props) => {
-  return (
-    <div>
-      <h3>Spot Details</h3>
-      <ResultItem />
-      <h3>Payment Info</h3>
-      <CardItemRender />
-      <Link to="/renter"><button>Buy Spot!</button></Link>
-    </div>
-    );
-};
 
-export default BuyItem;
+// const BuyItem = (props) => {
+//   return (
+//     <div>
+//       <h3>Spot Details</h3>
+//       <ResultItem />
+//       <h3>Payment Info</h3>
+//       <CardItemRender />
+//       <Link to="/renter"><button>Buy Spot!</button></Link>
+//     </div>
+//     );
+// };
+//
+// export default BuyItem;
