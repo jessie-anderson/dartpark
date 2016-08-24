edbf90d8062b0b8b13d1e423b0966760b2ed93ed
pinkluna <luisavasquez@luisas-mbp.wireless.dartmouth.edu>
2016-08-23 22:53:15 -0400

    removed redundant navbars

--- src/components/renter/searchbar.js
+++ src/components/renter/searchbar.js
@@ -4,7 +4,6 @@
 import React, { Component } from 'react';
 import Geosuggest from 'react-geosuggest';
 import { Link } from 'react-router';
-import NavBar from './navbar';
 import { saveSearch } from '../../actions/spot-actions';
 import { connect } from 'react-redux';
 
@@ -45,7 +44,6 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-        <NavBar />
         <h1>Find a Spot</h1>
         <div id="search">
           <div id="searchbar"><Geosuggest onSuggestSelect={this.suggestionSelection} onChange={this.onSearchBarChange} /></div>
