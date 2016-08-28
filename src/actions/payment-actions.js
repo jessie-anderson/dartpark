920b2b859d7f491ad89ac1d104b3092526c67ac8
mauesrog <Mauricio.Esquivel.Rogel.18@Dartmouth.edu>
2016-08-28 00:13:52 -0400

    corrected urls

--- src/actions/payment-actions.js
+++ src/actions/payment-actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const ROOT_URL = 'http://localhost:9090/api';
-// const ROOT_URL = 'http://dartpark.herokuapp.com/api'; // for when it's connected to the server
+// const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'http://dartpark.herokuapp.com/api'; // for when it's connected to the server
 import { browserHistory } from 'react-router';
 
 export const PaymentActionTypes = {
