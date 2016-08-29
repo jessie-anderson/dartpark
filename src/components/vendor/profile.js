563fa14554ba12b44b4173581590d599779606c0
pinkluna <luisavasquez@luisas-mbp.wireless.dartmouth.edu>
2016-08-29 11:00:14 -0400

    vendor profile styling

--- src/components/vendor/profile.js
+++ src/components/vendor/profile.js
@@ -17,8 +17,6 @@ class Profile extends Component {
     this.onChangeBio = this.onChangeBio.bind(this);
     this.saveChanges = this.saveChanges.bind(this);
     this.onEditClick = this.onEditClick.bind(this);
-    this.renderSpots = this.renderSpots.bind(this);
-    this.renderCars = this.renderCars.bind(this);
   }
 
   componentWillMount() {
@@ -41,73 +39,37 @@ class Profile extends Component {
     this.props.changeVendorBioAndName(this.state.bio, this.state.username);
     this.setState({ isEditing: !this.state.isEditing });
   }
-
-  renderSpots() {
-    if (this.props.spots === undefined) {
-      return (
-        <p>Loading spots...</p>
-      );
-    } else {
-      return this.props.spots.map(spot => {
-        return (
-          <SpotItem
-            id={spot._id}
-            key={spot._id}
-            address={spot.address}
-            price={spot.price}
-            startDate={spot.startDate}
-            endDate={spot.endDate}
-          />
-        );
-      });
-    }
-  }
-
-  renderCars() {
-    if (this.props.cars === undefined) {
-      return (
-        <p>Loading cars...</p>
-      );
-    } else {
-      return this.props.cars.map(car => {
-        console.log(car);
-        return (
-          <VehicleItem
-            _id={car._id}
-            key={car._id}
-            make={car.make}
-            model={car.model}
-            year={car.year}
-            paintcolor={car.paintcolor}
-          />
-        );
-      });
-    }
-  }
-
   render() {
-    const spots = this.renderSpots();
-    const cars = this.renderCars();
     let profile;
     if (!this.state.isEditing) {
       profile = (
-        <div>
-          <h1>Profile Information</h1>
-          <p>Name: {this.props.username}</p>
-          <p>Bio: {this.props.bio}</p>
-          <button onClick={this.onEditClick}>Edit Profile</button>
+        <div id="general-style">
+          <div id="center-content">
+            <div id="renter-bio">
+              <h1>Profile Information</h1>
+              <p>Name: {this.props.username}</p>
+              <p>Bio: {this.props.bio}</p>
+              <button id="std-btn" onClick={this.onEditClick}>Edit Profile</button>
+            </div>
+          </div>
         </div>
       );
     } else {
       profile = (
-        <div>
-          <h1>Profile Information</h1>
-          <label htmlFor="name">Name:</label>
-          <input value={this.state.username} onChange={this.onChangeUsername} id="name" />
-          <label htmlFor="bio">Bio:</label>
-          <input value={this.state.bio} onChange={this.onChangeBio} id="bio" />
-          <button onClick={this.onEditClick}>Cancel</button>
-          <button onClick={this.saveChanges}>Save Changes</button>
+        <div id="general-style">
+          <div id="center-content">
+            <h1>Profile Information</h1>
+            <div>
+              <label htmlFor="name">Name:</label>
+              <input value={this.state.username} onChange={this.onChangeUsername} id="name" />
+            </div>
+            <div>
+              <label htmlFor="bio">Bio:</label>
+              <input value={this.state.bio} onChange={this.onChangeBio} id="bio" />
+            </div>
+            <button id="std-btn" onClick={this.onEditClick}>Cancel</button>
+            <button id="std-btn" onClick={this.saveChanges}>Save Changes</button>
+          </div>
         </div>
       );
     }
