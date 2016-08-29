fe3f421cf129ff50b2521368f70e73689136f2f3
pinkluna <luisavasquez@luisas-mbp.wireless.dartmouth.edu>
2016-08-29 16:19:11 -0400

    fixed merge conflict

--- src/components/renter/spot-list-item.js
+++ src/components/renter/spot-list-item.js
@@ -23,7 +23,6 @@ class ResultItem extends Component {
   render() {
     console.log(this.props.address);
     return (
-<<<<<<< HEAD
       <div id="general-style">
         <div id="buySpotDisp">
           <h1>Address: {this.props.address}</h1>
@@ -37,23 +36,6 @@ class ResultItem extends Component {
             <button onClick={this.getSpotDetail}>Buy Spot</button>
           </Link>
         </div>
-
-=======
-      <div>
-        <ul className="result-details">
-          <li className="address">Address: {this.props.address}</li>
-          <li>Distance: {this.props.distance}</li>
-          <li>Vendor: {this.props.vendor}</li>
-          <li>Price: {this.props.price}</li>
-          <li>Start date: {this.props.startDate}</li>
-          <li>End date: {this.props.endDate}</li>
-          <li>Picture: {this.renderSpotPhoto}</li>
-          <img src={'http://greport.gru.edu/wp-content/uploads/2013/12/parkingspot.jpg'} alt="video" />
-        </ul>
-        <Link to="/renter/buy-spot">
-          <button onClick={this.getSpotDetail}>Buy Spot</button>
-        </Link>
->>>>>>> c19db5ea03a604cc69d0d8de333abba5abaa37bb
       </div>
     );
   }
